e5fd2ec9c11d01cc226ee4444470faf075433945
AlecCross <cross2472@gmail.com>
2021-02-15 17:50:02 +0200

    hw-2 add portals

---
+++ New Unity Project/Assets/Scripts/PortalJump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalJump : MonoBehaviour
+{
+    
+
+     void OnTriggerEnter(Collider collider){
+       if (collider.gameObject.name == "Portal") {  
+            gameObject.transform.position = GameObject.Find("Portal2").transform.position+ new Vector3(1, 0, 0);
+        }
+        else if(collider.gameObject.name == "Portal2") {  
+            gameObject.transform.position = GameObject.Find("Portal").transform.position + new Vector3(-1, 0, 0);
+        }
+        else if(collider.gameObject.name == "Portal3") {  
+            gameObject.transform.position = GameObject.Find("Portal4").transform.position + new Vector3(-2, 0, 0);
+        }
+        else if(collider.gameObject.name == "Portal4") {  
+            gameObject.transform.position = GameObject.Find("Portal3").transform.position + new Vector3(2, 0, 0);
+        }
+   }
+}
