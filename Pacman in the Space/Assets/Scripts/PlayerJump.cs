288d87da6ecadf228c744ab3cadd199fcf25267b
AlecCross <cross2472@gmail.com>
2021-02-21 12:55:14 +0200

    hw-3.1.1 API update

---
+++ Pacman in the Space/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    GameObject player;
+    private void Awake(){
+        
+    }
+    void Start(){
+        _rigidbody = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {//Попытка присечь подпрыгивание выще двух-трёх прыжков за один раз
+        if(player.transform.position.y <= 0.55){
+            if(Input.GetKeyDown(KeyCode.Space)){
+            _rigidbody.AddForce(Vector3.up * 350f * 0.02f, ForceMode.VelocityChange);
+//         rigidbody.AddForce(Vector3.up * 300);
+            } 
+        }    
+    }
+}
---
+++ Pacman in the Space/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    public float charSpeed = 5f;
+    public float turnSpeed = 50f;
+
+    void FixedUpdate()
+    //фиксированная частота срабатываний
+    {
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.JoystickButton19))
+            transform.Translate(Vector3.forward*charSpeed*Time.deltaTime);
+        if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.JoystickButton18))
+            transform.Translate(Vector3.back*charSpeed*Time.deltaTime);
+        if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.JoystickButton17))
+            transform.Translate(Vector3.left*charSpeed*Time.deltaTime);
+        if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.JoystickButton16))
+            transform.Translate(Vector3.right*charSpeed*Time.deltaTime);
+    //Vector3 это значит что точка находится в 3х мерном пространстве
+    }
+}
